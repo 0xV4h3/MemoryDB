954952d234395be692df9e09497bc7b2d47dfd20
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-27 21:10:48 +0400

    JsonSerializerStrategy added

---
+++ Engine/Serialization/JsonSerializerStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Core;
+
+namespace Engine.Serialization;
+
+public class JsonSerializerStrategy : IStorageSerializer
+{
+    private readonly JsonSerializerOptions _options = new() { WriteIndented = true };
+
+    public string SupportedExtension => ".json";
+
+    public byte[] Serialize<T>(T data) where T : class =>
+        JsonSerializer.SerializeToUtf8Bytes(data, _options);
+
+    public T? Deserialize<T>(byte[] bytes) where T : class =>
+        JsonSerializer.Deserialize<T>(bytes, _options);
+}
