982219d3733a6a5f9f923a1ae2441fa102371c96
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-27 21:10:01 +0400

    Serialization strategies interface

---
+++ Core/IStorageSerializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Core;
+
+public interface IStorageSerializer
+{
+    byte[] Serialize<T>(T data) where T : class;
+    
+    T? Deserialize<T>(byte[] bytes) where T : class;
+    
+    string SupportedExtension { get; }
+}
