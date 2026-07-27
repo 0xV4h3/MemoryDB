5d80f95dd2c7bcfd65e6b73de801750364664954
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-27 22:00:47 +0400

    Xml Serialization Strategy

---
+++ Engine/Serialization/XmlSerializerStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Serialization;
+using Core;
+
+namespace Engine.Serialization;
+
+public class XmlSerializerStrategy : IStorageSerializer
+{
+    private static readonly Dictionary<Type, XmlSerializer> _serializerCache = [];
+
+    public string SupportedExtension => ".xml";
+
+    public byte[] Serialize<T>(T data) where T : class
+    {
+        using var ms = new MemoryStream();
+        GetSerializer(typeof(T)).Serialize(ms, data);
+        return ms.ToArray();
+    }
+
+    public T? Deserialize<T>(byte[] bytes) where T : class
+    {
+        using var ms = new MemoryStream(bytes);
+        return (T?)GetSerializer(typeof(T)).Deserialize(ms);
+    }
+
+    private static XmlSerializer GetSerializer(Type type)
+    {
+        if (_serializerCache.TryGetValue(type, out var cached)) return cached;
+
+        var created = new XmlSerializer(type);
+        _serializerCache[type] = created;
+        return created;
+    }
+}
