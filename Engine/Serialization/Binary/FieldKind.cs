4c5372b80c70f2258835c3db0697e225008dbc40
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-28 20:52:07 +0400

    FieldKind for type classification added

---
+++ Engine/Serialization/Binary/FieldKind.cs
@@ -0,0 +1,15 @@
+﻿namespace Engine.Serialization.Binary;
+
+internal enum FieldKind
+{
+    Primitive,
+    String,
+    Guid,
+    DateTime,
+    TimeSpan,
+    Enum,
+    Nullable,
+    Array,
+    List,
+    Nested
+}
---
+++ Engine/Serialization/Binary/FieldKindClassifier.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace Engine.Serialization.Binary;
+
+internal class FieldKindClassifier
+{
+    public static (FieldKind Kind, Type? ElementType, Type? UnderlyingType) Classify(Type type)
+    {
+        if (type == typeof(string)) return (FieldKind.String, null, null);
+        if (type == typeof(Guid)) return (FieldKind.Guid, null, null);
+        if (type == typeof(DateTime)) return (FieldKind.DateTime, null, null);
+        if (type == typeof(TimeSpan)) return (FieldKind.TimeSpan, null, null);
+
+        var nullableUnderlying = Nullable.GetUnderlyingType(type);
+        if (nullableUnderlying != null) return (FieldKind.Nullable, null, nullableUnderlying);
+
+        if (type.IsEnum) return (FieldKind.Enum, null, Enum.GetUnderlyingType(type));
+        if (type.IsPrimitive || type == typeof(decimal)) return (FieldKind.Primitive, null, null);
+
+        if (type.IsArray) return (FieldKind.Array, type.GetElementType(), null);
+
+        if (type.IsGenericType && typeof(IEnumerable).IsAssignableFrom(type))
+        {
+            var elementType = type.GetGenericArguments().FirstOrDefault() ?? typeof(object);
+            return (FieldKind.List, elementType, null);
+        }
+
+        return (FieldKind.Nested, null, null);
+    }
+}
