b2930428964bcf4e3fdb39b7c29f2e01759524b0
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-06 20:36:47 +0400

    SupportedExtension removed from Serializer interface

--- Core/IStorageSerializer.cs
+++ Core/IStorageSerializer.cs
@@ -5,6 +5,4 @@ public interface IStorageSerializer
     byte[] Serialize<T>(T data) where T : class;
     
     T? Deserialize<T>(byte[] bytes) where T : class;
-    
-    string SupportedExtension { get; }
 }
