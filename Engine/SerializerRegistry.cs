ef16324a2436a078acf98fce30071c7f68bc4fec
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-06 20:45:38 +0400

    SerializerRegistry created

---
+++ Engine/SerializerRegistry.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+
+namespace Engine;
+
+public sealed class SerializerRegistry
+{
+    private readonly Dictionary<string, IStorageSerializer> _byExtension = [];
+
+    public SerializerRegistry Register(string extension, IStorageSerializer serializer)
+    {
+        _byExtension[extension] = serializer;
+        return this;
+    }
+
+    public IEnumerable<(string Extension, IStorageSerializer Serializer)> All =>
+        _byExtension.Select(kvp => (kvp.Key, kvp.Value));
+
+    public IStorageSerializer? GetByExtension(string extension) =>
+        _byExtension.GetValueOrDefault(extension);
+}
