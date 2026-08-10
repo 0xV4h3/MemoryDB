96b8466e926432047b91dc87f0b8cb3823b7a02a
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-10 17:39:04 +0400

    New approach of SerializerTest configuration with Serializers class

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ class Program
         var config = TestConfig.CreateDefault();
 
         var runner = new TestRunner(config)
-            .Add(new XmlSerializerTest());
+            .Add(new SerializerTest(Serializers.Xml));
 
         var results = runner.RunAll();
 
