ed94bac365094b64cd88d83ef31fd3e37a76c51c
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-10 16:25:30 +0400

    Type and Property accessors done

---
+++ Engine/Serialization/Binary/PropertyAccessor.cs
@@ -0,0 +1,13 @@
+﻿namespace Engine.Serialization.Binary;
+
+internal sealed class PropertyAccessor
+{
+    public required string Name { get; init; }
+    public required Type PropertyType { get; init; }
+    public required FieldKind Kind { get; init; }
+    public Type? ElementType { get; init; }
+    public Type? UnderlyingType { get; init; }
+
+    public required Func<object, object?> Getter { get; init; }
+    public required Action<object, object?> Setter { get; init; }
+}
---
+++ Engine/Serialization/Binary/TypeAccessorPlan.cs
@@ -0,0 +1,7 @@
+﻿namespace Engine.Serialization.Binary;
+
+internal sealed class TypeAccessorPlan
+{
+    public required Type Type { get; init; }
+    public required PropertyAccessor[] Properties { get; init; }
+}
