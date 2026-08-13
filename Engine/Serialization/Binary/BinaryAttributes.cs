6f828226fcf2cafc9819fbcb92fe9e3383efa871
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-13 16:19:34 +0400

    BinaryIgnoreAttribute and BinaryOrderAttribute added

---
+++ Engine/Serialization/Binary/BinaryAttributes.cs
@@ -0,0 +1,10 @@
+﻿namespace Engine.Serialization.Binary;
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class BinaryIgnoreAttribute : Attribute;
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class BinaryOrderAttribute(int order) : Attribute
+{
+    public int Order { get; } = order;
+}
