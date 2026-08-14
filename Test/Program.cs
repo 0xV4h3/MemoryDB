3f2b07b573bb69de6130376c960e0dc442840f89
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-14 16:03:02 +0400

    DefaultBinary used in testing

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ class Program
         var config = TestConfig.CreateDefault();
 
         var runner = new TestRunner(config)
-            .Add(new SerializerTest(Serializers.Binary))
+            .Add(new SerializerTest(Serializers.DefaultBinary))
             .Add(new SerializerTest(Serializers.Json))
             .Add(new SerializerTest(Serializers.Xml));
 
