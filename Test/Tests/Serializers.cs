4ea7b07e65b77a3c0b060d62cf7260856f607e32
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-15 13:41:10 +0400

    Binary serializer providers for testing updated

--- Test/Tests/Serializers.cs
+++ Test/Tests/Serializers.cs
@@ -2,19 +2,22 @@
 using Engine.Serialization;
 using Engine.Serialization.Binary;
 using Engine.Serialization.Binary.Compression;
+using Engine.Serialization.Binary.Checksum;
 
 namespace Test.Tests;
 
 public static class Serializers
 {
     public static (string name, string extension, IStorageSerializer serializer) DefaultBinary
-        => ("BinarySerializer", ".bin", new BinarySerializerStrategy(new Compressor(new NoCompression())));
+        => ("BinarySerializer", ".bin", new BinarySerializerStrategy());
     
     public static (string name, string extension, IStorageSerializer serializer) Binary(
-        ICompressionStrategy compressionStrategy)
+        ICompressionStrategy? compressionStrategy = null,
+        IIntegrityChecksum? checksum = null)
     {
-        var compressor = new Compressor(compressionStrategy);
-        var serializer = new BinarySerializerStrategy(compressor);
+        var compressor = new Compressor(compressionStrategy ?? new NoCompression());
+        var checksumProvider = new ChecksumProvider(checksum ?? new Crc32Checksum());
+        var serializer = new BinarySerializerStrategy(compressor, checksumProvider);
         return ("BinarySerializer", ".bin", serializer);
     }
     
