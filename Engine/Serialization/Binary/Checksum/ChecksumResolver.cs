242d1bc6f168b9d586a8edb99c1fb1f266509401
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-15 13:10:19 +0400

    Checksum Algorithms resolver

---
+++ Engine/Serialization/Binary/Checksum/ChecksumResolver.cs
@@ -0,0 +1,10 @@
+﻿namespace Engine.Serialization.Binary.Checksum;
+
+internal static class ChecksumResolver
+{
+    public static IIntegrityChecksum Resolve(ChecksumAlgorithm algorithm) => algorithm switch
+    {
+        ChecksumAlgorithm.Crc32 => new Crc32Checksum(),
+        _ => throw new NotSupportedException($"Unknown checksum algorithm in file header: {algorithm}")
+    };
+}
